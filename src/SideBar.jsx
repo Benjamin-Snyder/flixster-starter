13b9b772f56eb56ba2502a7950c6bc1a69d80142
Ben Snyder <bensnyder@meta.com>
2025-06-12 12:03:53 -0700

    [Flixster][Index] style sidebar

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './Sidebar.css';
 
 const SideBar = ({ onClick, toggleFavoritesView, toggleSeenView, toggleHomeView}) => {
+
+    
+
     return (
-        <div className="sidebar" onClick={onClick}>
-            <button onClick={toggleHomeView}><img className="homeNav" src="./src/assets/home.png" alt="Home Navigation" /></button>
-            <button onClick={toggleFavoritesView}><img className="favNav" src="./src/assets/favorite-button-black.png" alt="Favorite Movie Navigation" /></button>
-            <button onClick={toggleSeenView}><img className="seenNav" src="./src/assets/seen-black.png" alt="Seen Navigation" /></button>
+        <div className="sideBar" onClick={onClick}>
+            <div className="navButtons">
+                <img onClick={toggleHomeView} className="homeNav" src="./src/assets/home.png" alt="Home Navigation" />
+                <img onClick={toggleFavoritesView} className="favNav" src="./src/assets/favorite-button-black.png" alt="Favorite Movie Navigation" />
+                <img onClick={toggleSeenView} className="seenNav" src="./src/assets/seen-black.png" alt="Seen Navigation" />
+            </div>
+
+
         </div>
     );
 };
